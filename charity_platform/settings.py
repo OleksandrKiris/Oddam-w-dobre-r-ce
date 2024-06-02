a0814058d1ffdc17730e492924ef05f40d9b27da
Oleksandr <oleksandr.kiris@icloud.com>
2024-06-02 11:38:46 +0200

    Setup virtual environment, installed necessary packages, created database, modified settings, ran first migration, created app and added to settings, project is running

--- charity_platform/settings.py
+++ charity_platform/settings.py
@@ -1,21 +1,21 @@
 from pathlib import Path
 from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Budowanie ścieżek w projekcie, takich jak: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# Szybkie ustawienia deweloperskie - nieodpowiednie do produkcji
+# Zobacz https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# OSTRZEŻENIE: Trzymaj klucz bezpieczeństwa używany w produkcji w tajemnicy!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# OSTRZEŻENIE: Nie uruchamiaj z włączonym debugowaniem w produkcji!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
-# Application definition
+# Definicja aplikacji
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,8 +57,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'charity_platform.wsgi.application'
 
-
-# Database
+# Baza danych
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
@@ -72,8 +71,7 @@ DATABASES = {
     }
 }
 
-
-# Password validation
+# Walidacja haseł
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -91,8 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
+# Internacjonalizacja
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
@@ -103,13 +100,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
+# Pliki statyczne (CSS, JavaScript, Obrazy)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+# Domyślny typ klucza podstawowego
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
